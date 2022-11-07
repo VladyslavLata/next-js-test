a9223f9639651158af6cea949fc97be0aa850eee
VladyslavLata <vladlatahtc@gmail.com>
2022-11-07 18:17:25 +0200

    add footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
+import stylesApp from "../../styles/Home.module.scss";
+import styles from "./Footer.module.scss";
 
-export const Footer:FC = () => {
+export const Footer: FC = () => {
   return (
-    <h1>FOOTER</h1>
-  )
-} 
+    <footer className={styles.footer}>
+      <div className={stylesApp.container}>
+        <p className={styles.text}> &copy; Footer info </p>
+      </div>
+    </footer>
+  );
+};
