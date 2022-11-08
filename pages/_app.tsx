c25b498e773c243e476044fc9fb1d6f4ab94abad
VladyslavLata <vladlatahtc@gmail.com>
2022-11-08 17:10:43 +0200

    recode add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,14 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;500;600;700;900&display=swap"
           rel="stylesheet"
         />
       </Head>
