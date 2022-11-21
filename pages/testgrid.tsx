1b988dcfd799f2522733010214cc5fa20bc019fe
VladyslavLata <vladlatahtc@gmail.com>
2022-11-21 18:30:03 +0200

    add svg

--- pages/testgrid.tsx
+++ pages/testgrid.tsx
@@ -1,13 +1,19 @@
 import { ListGridTest } from "../components/ListGridTest/ListGrid";
 import { Container } from "../components/Container/Container";
 import { Section } from "../components/Section/Section";
+import  svg  from "../public/svg.svg";
+// import Image from "next/image";
 
 const TestGrid = () => {
   return (
     <Section>
       <Container>
-  <ListGridTest/></Container>
-  </Section>
+        <ListGridTest />
+        <svg width={30} height={30}>
+          <use href={svg+"#icon-mob-menu-close"}></use>
+        </svg>
+      </Container>
+    </Section>
   );
 };
 
